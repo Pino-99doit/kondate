c1cc4dc71c1d7f7d2668b218c6687b05e4a7381f
Pino-99doit <reityu44010@gmail.com>
2026-06-28 14:09:24 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* オフライン用キャッシュ。アプリ一式を端末に保存し、ネットなしでも起動できるようにする。 */
-const CACHE='kondate-v7';
+const CACHE='kondate-v8';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
